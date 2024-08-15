e39d07df3a55b97f816586fa7a35138a1343842f
sahil-shelar <adesh.shelar@outlook.com>
2024-08-16 01:11:05 +0530

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure all relevant file types are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
